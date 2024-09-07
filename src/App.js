1a658bf9dc3262a5da29a1e771e481b3de3dd934
SangWon7242 <gotkdqja@gmail.com>
2024-09-07 15:29:48 +0900

    할 일 추가 간단한 UI 작성

--- src/App.js
+++ src/App.js
@@ -2,7 +2,27 @@ import React, { useState } from "react";
 import "./App.css";
 
 function App() {
-  return <div>안녕</div>;
+  const [newTodoTitle, setNewTodoTitle] = useState("");
+
+  return (
+    <>
+      <h1>새 할일</h1>
+      &nbsp;
+      <input
+        type="text"
+        placeholder="새 할일을 입력해주세요."
+        value={newTodoTitle}
+        onSubmit={(e) => setNewTodoTitle(e.target.value)}
+      />
+      &nbsp;
+      <button onClick={() => setNewTodoTitle("11")}>할 일 추가</button>
+      <nav>
+        <ul>
+          <li>할 일 목록</li>
+        </ul>
+      </nav>
+    </>
+  );
 }
 
 export default App;
