e6b264ff533d903c45e82826fd6d0f47f8cbba4e
SangWon7242 <gotkdqja@gmail.com>
2024-09-09 22:02:09 +0900

    입력시 할일 표시

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 
 function App() {
   const [newTodoTitle, setNewTodoTitle] = useState("");
+  const [inputedTotoTitle, setInputedTotoTitle] = useState("");
 
   return (
     <>
@@ -12,15 +13,31 @@ function App() {
         type="text"
         placeholder="새 할일을 입력해주세요."
         value={newTodoTitle}
-        onSubmit={(e) => setNewTodoTitle(e.target.value)}
+        onChange={(e) => setNewTodoTitle(e.target.value)}
       />
       &nbsp;
-      <button onClick={() => setNewTodoTitle("11")}>할 일 추가</button>
-      <nav>
-        <ul>
-          <li>할 일 목록</li>
-        </ul>
-      </nav>
+      <button
+        onClick={() => {
+          setNewTodoTitle("");
+          setInputedTotoTitle(newTodoTitle);
+        }}
+      >
+        할 일 추가
+      </button>
+      <div>
+        {inputedTotoTitle === "" ? (
+          <h2>할 일 등록 필요</h2>
+        ) : (
+          <>
+            <h2>할 일 목록</h2>
+            <nav>
+              <ul>
+                <li>{inputedTotoTitle}</li>
+              </ul>
+            </nav>
+          </>
+        )}
+      </div>
     </>
   );
 }
